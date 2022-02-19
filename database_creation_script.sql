cf1b3fddff0b645371e04f7a5a5516c47bf427ce
Matheus de Souza <mateuspapa129@gmail.com>
2022-02-19 09:34:44 -0300

    Modificação no nome do BD

--- database_creation_script.sql
+++ database_creation_script.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `Eitus`
+-- Banco de dados: `Exitus`
 --
 
 -- --------------------------------------------------------
